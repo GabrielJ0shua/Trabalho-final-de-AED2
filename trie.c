14c766891cb62603c58bb5bc751e9e17f9871e8b
Gabriel Joshua <gjoshuacalixto@outlook.com>
2021-05-29 14:55:10 -0300

    esqueleto
    fase inicial do projeto

---
+++ trie.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "tad.h"
+
+Trie* criaTrie(){
+
+}
+
+void liberaTrie(Trie* tr){
+
+}
+
+int insereTrie(Trie* tr, char *str){
+
+}
+
+int buscaTrie(Trie* tr, char *str){
+
+}
+
+int removeTrie(Trie* tr, char *str){
+
+}
+
+void imprimeTrie(Trie* tr){
+
+}
+
+void autocompletarTrie(Trie* tr, char *prefixo){
+    
+}
